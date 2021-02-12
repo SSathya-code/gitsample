2a27669201808d8ad8fb564078ebc8eb817f5112
SSathya-code <75477186+SSathya-code@users.noreply.github.com>
2021-02-12 14:20:22 +0800

    Update HelloIntelli.java

--- src/HelloIntelli.java
+++ src/HelloIntelli.java
@@ -1,7 +1,8 @@
 public class HelloIntelli {
     //added comment for intellij
+    // check for intellj
     public static void main(String args[]){
-      //  System.out.println("welcome");
-          System.out.println("hai");
+      System.out.println("welcome");
+       //   System.out.println("hai");
     }
 }
